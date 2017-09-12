431cd006cbbfc69d30e18ce10a7180be97ef20f9
sirisha_a <sirisha_a@DS-A101AA310705.HCLT.CORP.HCL.IN>
2017-09-12 12:16:30 +0530

    commit by sirisha 1

---
+++ gitbuilddemo/src/gitbuildtest/PrintDemo.java
@@ -0,0 +1,10 @@
+package gitbuildtest;
+
+public class PrintDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a Jenkins build demo 2");
+	}
+
+}
